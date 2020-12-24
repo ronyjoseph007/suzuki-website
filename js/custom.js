5f4f0deda416413e16e1470eed562ed728a198d2
ronyjoseph007 <ronyjoseph1998@gmail.com>
2020-12-23 20:39:38 -0800

    edited

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 0,
+    
     effect:'cube',
     speed:1000,
     loop:true,
-    autoplay:{
-        delay:3000,
-    },
+    // autoplay:{
+    //     delay:3000,
+    // },
     freemode:true,
     pagination: {
       el: '.swiper-pagination',
